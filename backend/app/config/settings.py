a0cad3932c394c5b57079fc592cd3e3757baea84
bhavinmokariya <bhavinmokariya@simprosys.org>
2026-04-10 12:27:03 +0530

    settings change

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,16 +1,18 @@
 from pydantic_settings import BaseSettings
 import os
-import load_dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = "your-super-secret-key-change-this-in-production"
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-    MONGODB_URL: str = "mongodb://localhost:27017"
-    DATABASE_NAME: str = "ticket_support_db"
-    CUSTOMER_BACKEND_URL: str = "http://localhost:8000"
+    MONGODB_URL: str = os.getenv("MONGODB_URL")
+    DATABASE_NAME: str = os.getenv("DATABASE_NAME")
+    CUSTOMER_BACKEND_URL: str = os.getenv("CUSTOMER_BACKEND_URL")
 
     class Config:
         env_file = ".env"
